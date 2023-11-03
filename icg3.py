0a45f8cb8fbadc934f24f7d477932f8569b49b53
Leonardo <patinhoquackk42@gmail.com>
2023-11-03 11:21:32 -0300

    Add files via upload

--- icg3.py
+++ icg3.py
@@ -15,6 +15,7 @@ CASTLE_COLOR = (255, 0, 0)
 BARRACKS_COLOR = (0, 128, 0)
 ENEMY_COLOR = (0, 0, 255)
 FPS = 30
+MAX_GUARDS_PER_BARRACK = 4
 
 # Load and resize images
 castle_img = pygame.transform.scale(pygame.image.load("castle.png"), (50, 50))
@@ -38,13 +39,16 @@ barracks = []  # Store barracks as [x, y, size]
 barracks_cost = 100
 barracks_spawn_rate = 2
 
-guards = []  # Store guards as [x, y, size, hp, damage]
-guard_damage = 20
-guard_cooldown = 2  # Add a cooldown for guard attacks
+guards = []  # Store guards as [x, y, size, hp, damage, cooldown_timer]
+barracks_positions = []
+guard_dmg = 20
+guard_cooldown = 1
+guard_cooldown_timer = 0
 
 enemies = []  # Store enemies as [x, y, size, hp, damage]
-enemy_damage = 5
+enemy_damage = 1
 enemy_spawn_interval = 2
+enemy_hp = 30
 
 enemy_xp_reward = 10
 level = 1
@@ -54,7 +58,8 @@ xp_drop_enabled = True
 XP_INCREMENT_PER_LEVEL = 100
 xp_needed_for_next_level = level * XP_INCREMENT_PER_LEVEL
 
-dx, dy = 0, 0
+step_size = 2 
+
 
 # Load the font
 font = pygame.font.Font(None, 36)
@@ -73,9 +78,8 @@ last_enemy_spawn_time = time.time()
 
 #def's
 def draw_castle():
-    pygame.draw.rect(screen, CASTLE_COLOR, (castle_x - castle_size // 2, castle_y - castle_size // 2, castle_size, castle_size))
-    pygame.draw.rect(screen, (0, 0, 0), (castle_x - castle_size // 2, castle_y - castle_size // 2, castle_size, 5))
-    screen.blit(castle_img), (castle_x - castle_size // 2, castle_y - castle_size // 2)
+    pygame.draw.rect(screen, (255, 0, 0), (castle_x - castle_size // 2, castle_y - castle_size // 2, castle_size, castle_size))
+    screen.blit(castle_img, (castle_x - castle_size // 2, castle_y - castle_size // 2))
 
 def draw_info():
     text = font.render(f"Castle Level: {castle_level}", True, (0, 0, 0))
@@ -107,11 +111,39 @@ def draw_upgrade_castle_button():
     text = font.render("Upgrade Castle", True, (0, 0, 0))
     screen.blit(text, castle_upgrade_button.move(0, 0))
 
+def draw_barracks():
+    for barrack in barracks:
+        x, y, size, remaining_guards = barrack  # Unpack x, y, size, and remaining_guards
+        pygame.draw.rect(screen, BARRACKS_COLOR, (x - size // 2, y - size // 2, size, size))
+        screen.blit(barracks_img, (x - size // 2, y - size // 2))
+
+def draw_guards():
+    for guard in guards:
+        x, y, size, hp, damage, cooldown_timer = guard
+        pygame.draw.rect(screen, (0, 255, 0), (x - size // 2, y - size // 2, size, size))
+        screen.blit(guard_img, (x - size // 2, y - size // 2))
+
+def create_barracks(castle_gold):
+    x, y = random.randint(castle_x - 100, castle_x + 100), random.randint(castle_y - 100, castle_y + 100)
+    if castle_x - 100 <= x <= castle_x + 100 and castle_y - 100 <= y <= castle_y + 100:
+        for _ in range(4):  # Create 4 guards per barracks
+            guards.append([x, y, castle_size, 100, guard_dmg, 0])  # Cooldown timer starts at 0
+        castle_gold -= barracks_cost
+    return castle_gold
+
+def draw_enemies():
+    for enemy in enemies:
+        enemy_x, enemy_y, enemy_size, enemy_hp, enemy_dmg = enemy
+        pygame.draw.rect(screen, ENEMY_COLOR, (enemy_x - enemy_size // 2, enemy_y - enemy_size // 2, enemy_size, enemy_size))
+        screen.blit(enemy_img, (enemy_x - enemy_size // 2, enemy_y - enemy_size // 2))
+
+
 
 # Main game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
@@ -120,7 +152,8 @@ while True:
                 enemies = []
                 castle_gold = 0
                 castle_level = 1
-                # Reset other variables as needed
+                barracks = []
+                barracks_positions = []
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if castle_upgrade_button.collidepoint(event.pos):
@@ -132,9 +165,7 @@ while True:
 
             if create_barracks_button.collidepoint(event.pos):
                 if castle_gold >= barracks_cost:
-                    x, y = random.randint(50, WIDTH - 50), random.randint(50, HEIGHT - 50)
-                    barracks.append([x, y, castle_size])
-                    castle_gold -= barracks_cost
+                    castle_gold = create_barracks(castle_gold)
 
     current_time = time.time()
 
@@ -147,15 +178,72 @@ while True:
     # Increment the castle's gold
     castle_gold += gold_generated
 
-    # Update game variables here
-
     # Implement barracks spawning guards
     if (current_time - last_guard_spawn_time) >= guard_cooldown:
         for barrack in barracks:
-            x, y, size = barrack
-            guards.append([x, y, size, 100, guard_damage])
+            x, y, size, remaining_guards = barrack
+            if remaining_guards > 0:
+                for _ in range(min(4, remaining_guards)):  # Spawn up to 4 guards per barracks
+                    guards.append([x, y, 100, guard_dmg, guard_cooldown])
+                    remaining_guards -= 1  # Reduce the remaining guards count for the barracks
         last_guard_spawn_time = current_time
 
+            # Count the number of guards spawned from this barrack
+        guards_spawned = 0
+
+            # Find available barrack positions
+        available_positions = list(barracks_positions)
+
+    for guard in guards:
+        guard_x, guard_y, guard_size, guard_hp, guard_dmg, guard_cooldown_timer = guard
+
+        # Check if the guard's cooldown timer is zero
+        if guard_cooldown_timer <= 0:
+            nearest_enemy = None
+            min_distance = float('inf')
+
+            for enemy in enemies:
+                enemy_x, enemy_y, enemy_size, enemy_hp, enemy_dmg = enemy
+                distance = math.sqrt((guard_x - enemy_x) ** 2 + (guard_y - enemy_y) ** 2)
+
+                if distance < min_distance:
+                    nearest_enemy = enemy
+                    min_distance = distance
+
+            if nearest_enemy is not None:
+                enemy_x, enemy_y, enemy_size, enemy_hp, enemy_dmg = nearest_enemy
+
+                # Calculate the direction vector
+                dx = enemy_x - guard_x
+                dy = enemy_y - guard_y
+
+                # Calculate the unit vector
+                distance = math.sqrt(dx ** 2 + dy ** 2)
+                if distance > 0:
+                    dx /= distance
+                    dy /= distance
+
+                # Increase the step size for faster movement
+                step_size = 2
+
+                # Move the guard in the direction of the nearest enemy
+                guard_x += dx * step_size
+                guard_y += dy * step_size
+
+                # Update the guard's position in the current guard list
+                guard[0] = guard_x
+                guard[1] = guard_y
+
+                # Update the guard's cooldown timer
+                guard_cooldown_timer = guard_cooldown
+
+        else:
+            # Decrease the guard's cooldown timer
+            guard_cooldown_timer -= time_elapsed
+
+    # Update the guard's cooldown timer
+    if guard_cooldown_timer > 0:
+        guard_cooldown_timer -= time_elapsed
     # Implement enemy spawning
     if (current_time - last_enemy_spawn_time) >= enemy_spawn_interval:
         x, y = random.randint(50, WIDTH - 50), random.randint(50, HEIGHT - 50)
@@ -163,17 +251,34 @@ while True:
         last_enemy_spawn_time = current_time
 
     # Update guards attacking enemies
-    for guard in guards:
-        for enemy in enemies:
-            guard_x, guard_y, guard_size, guard_hp, guard_dmg = guard
-            enemy_x, enemy_y, enemy_size, enemy_hp, enemy_dmg = enemy
-            distance = math.sqrt((guard_x - enemy_x) ** 2 + (guard_y - enemy_y) ** 2)
-            if distance <= guard_size / 2:
-                if enemy_hp > 0:
-                    enemy_hp -= guard_dmg
-                    if enemy_hp <= 0:
-                        enemies.remove(enemy)
-                        xp += enemy_xp_reward
+    for enemy in enemies:
+        enemy_x, enemy_y, enemy_size, enemy_hp, enemy_dmg = enemy
+        distance = math.sqrt((castle_x - enemy_x) ** 2 + (castle_y - enemy_y) ** 2)
+
+        if distance <= castle_size / 2:
+            if castle_hp > 0:
+                castle_hp -= enemy_dmg
+                if castle_hp <= 0:
+                    # Handle game over logic
+                    pass
+        else:
+            # Calculate the direction vector
+            dx = castle_x - enemy_x
+            dy = castle_y - enemy_y
+
+            # Calculate the unit vector
+            distance = math.sqrt(dx ** 2 + dy ** 2)
+            if distance > 0:
+                dx /= distance
+                dy /= distance
+
+            # Move the enemy in the direction of the castle
+            enemy_x += dx
+            enemy_y += dy
+
+            # Update the enemy's position
+            enemy[0] = enemy_x
+            enemy[1] = enemy_y
 
     # Update enemies attacking the castle
     for enemy in enemies:
@@ -185,7 +290,6 @@ while True:
                 if castle_hp <= 0:
                     # Handle game over logic
                     pass
-
     # Handle leveling up
     if xp >= xp_needed_for_next_level:
         xp -= xp_needed_for_next_level
@@ -202,6 +306,9 @@ while True:
     draw_info()
     draw_upgrade_castle_button()
     draw_create_barracks_button()
+    draw_barracks()
+    draw_guards()
+    draw_enemies()
 
     ####
     pygame.display.update()
